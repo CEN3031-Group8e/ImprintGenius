85cf392a778b0b4f5971dd6d052dcfabb5002794
Sneha P <snehashrey17@gmail.com>
2019-11-07 15:49:33 -0500

    upload image works and displays

--- client/src/components/Upload file/UploadFile.js
+++ client/src/components/Upload file/UploadFile.js
@@ -21,11 +21,23 @@ class UploadFile extends React.Component
 
     
     onClickHandler = () => {
-        const data = new FormData() 
+        /*const data = new FormData() 
         data.append('file', this.state.selectedFile)
                 //const image = this.state.selectedFile
                
-                console.log("image ", data)
+                console.log("image ", data)*/
+
+            var input = this.state.selectedFile    
+            var reader = new FileReader();
+            reader.onload = function()
+            {
+                var dataURL = reader.result;
+                var output = document.getElementById('output')
+                output.src = dataURL;
+
+            }
+
+            reader.readAsDataURL(input);
      }
 
     render() 
@@ -38,7 +50,7 @@ class UploadFile extends React.Component
                       
                       <div className="form-group files">
                         <label>Upload Your File </label>
-                        <input type="file" className="form-control" multiple="" onChange={this.onChangeHandler} />
+                        <input type="file"  className="form-control"  onChange={this.onChangeHandler}  />
                        {/*<input type="file" name="file" onchange={this.onChangeHandler}/>*/}
     
                        
@@ -51,6 +63,7 @@ class UploadFile extends React.Component
               
             </div>
             <button type="button" className="btn btn-success btn-block" onClick={this.onClickHandler}>Upload</button>
+            <img id = 'output'></img>
         </div>
     
         )
