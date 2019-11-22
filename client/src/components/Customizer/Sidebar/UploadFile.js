8b5fea43969eb63bb3613cc255fe9c9dde0d00cd
Sneha P <snehashrey17@gmail.com>
2019-11-22 17:50:29 -0500

    Upload different images for apparel and items

--- client/src/components/Customizer/Sidebar/UploadFile.js
+++ client/src/components/Customizer/Sidebar/UploadFile.js
@@ -9,7 +9,8 @@ class UploadFile extends React.Component
      {
         super(props);
         this.state = {
-            selectedFile: null
+            selectedFile: null,    //apparel logo
+            selectedFileTwo: null  //item logo
             //selectedImage: null
         }
     }
@@ -74,22 +75,109 @@ class UploadFile extends React.Component
        </div>)}
     }
 
+    
+    onChangeHandlerTwo=event=>
+    {
+        //setting state in app.js
+        if(event.target.files[0] != null)
+        {
+          this.setState({
+            selectedFileTwo: event.target.files[0]
+          })
+          //new
+          this.props.updateFileTwo( event.target.files[0])
+
+          try
+          {window.URL.createObjectURL(event.target.files[0])}
+          catch(error){
+            console.log('no chosen file')
+          }
+
+        }
+
+    }
+
+    onClickHandlerTwo = () => {	
+      console.log("clicked")	
+      var input = this.state.selectedFileTwo	
+      var reader = new FileReader();	
+
+
+      //define onload function	
+      //onload event is fired when the filereader has finished reading the file	
+      reader.onload = () =>	
+      {	
+          var dataURL = reader.result; //render.result contains resultant contents of the file	
+         // var output = document.getElementById('output') //get the area where you want the image to be displayed	
+         // output.src = dataURL; //set that image's src as the file's dataURL	
+          this.setState({	
+              selectedImageTwo: dataURL	
+            })	
+            this.props.updateImageTwo( dataURL) //pass dataURL to page2 parent	
+            //console.log('selectedImage ', this.state.selectedImage)	
+      }	
+      //read file. Once it is read, onload function will be called	
+      reader.readAsDataURL(input);	
+
+     }
+
+    allowUploadTwo()
+    {
+      if(this.state.selectedFileTwo === null){
+        return;
+      } else {
+
+      return (
+       <div>
+         <button type="button" className="btn btn-success btn"
+                onClick={this.onClickHandlerTwo}>
+                Upload
+        </button>
+      
+       </div>)}
+    }
+
+  
+
     render()
     {
+      if(this.props.apparelMode === true)
+      {
         return(
-            <div className="uploadBox">
-              <h1>Add an Image</h1>
-              <p>Upload your file</p>
-              <form className="uploadForm" method="post" action="#" id="#">
-                  <div className="form-group files">
-                    <input type="file"  className="form-control"
-                           accept = "image/*"
-                           onChange={this.onChangeHandler}/>
-                  </div>
-              </form>
-            {this.allowUpload()}
-            </div>
-        )
+          <div className="uploadBox">
+            <h1>Add an Image</h1>
+            <p>Upload your apparel logo file</p>
+            <form className="uploadForm" method="post" action="#" id="#">
+                <div className="form-group files">
+                  <input type="file"  className="form-control"
+                         accept = "image/*"
+                         onChange={this.onChangeHandler}/>
+                </div>
+            </form>
+          {this.allowUpload()}
+          </div>
+      )
+
+      }
+
+      else
+      {
+        return(
+          <div className="uploadBox" id = "#2">
+            <h1>Add an Image</h1>
+            <p>Upload your item logo file</p>
+            <form className="uploadForm" method="post" action="#" id="#2">
+                <div className="form-group files">
+                  <input type="file"  className="form-control"
+                         accept = "image/*"
+                         onChange={this.onChangeHandlerTwo}/>
+                </div>
+            </form>
+          {this.allowUploadTwo()}
+          </div>
+      )
+      }
+        
     }
   }
 
