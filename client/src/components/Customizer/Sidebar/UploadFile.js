0f32508a56454c662e8a19b43981e5766bdd5e1b
Sneha P <snehashrey17@gmail.com>
2019-11-22 10:18:18 -0500

    fixed delayed state and render issues using upload button

--- client/src/components/Customizer/Sidebar/UploadFile.js
+++ client/src/components/Customizer/Sidebar/UploadFile.js
@@ -10,6 +10,7 @@ class UploadFile extends React.Component
         super(props);
         this.state = {
             selectedFile: null
+            //selectedImage: null
         }
     }
     onChangeHandler=event=>
@@ -33,38 +34,46 @@ class UploadFile extends React.Component
 
     }
 
+    onClickHandler = () => {	
+      console.log("clicked")	
+      var input = this.state.selectedFile	
+      var reader = new FileReader();	
+
+
+      //define onload function	
+      //onload event is fired when the filereader has finished reading the file	
+      reader.onload = () =>	
+      {	
+          var dataURL = reader.result; //render.result contains resultant contents of the file	
+         // var output = document.getElementById('output') //get the area where you want the image to be displayed	
+         // output.src = dataURL; //set that image's src as the file's dataURL	
+          this.setState({	
+              selectedImage: dataURL	
+            })	
+            this.props.updateImage( dataURL) //pass dataURL to page2 parent	
+            //console.log('selectedImage ', this.state.selectedImage)	
+      }	
+      //read file. Once it is read, onload function will be called	
+      reader.readAsDataURL(input);	
+
+     }
+
     allowUpload()
     {
       if(this.state.selectedFile === null){
         return;
       } else {
 
-        var input = this.state.selectedFile;
-        var reader = new FileReader();
-
-        //define onload function
-        //onload event is fired when the filereader has finished reading the file
-        reader.onload = () =>
-        {
-            var dataURL = reader.result; //render.result contains resultant contents of the file
-            this.setState({
-                selectedImage: dataURL
-              })
-              this.props.updateImage( dataURL) //pass dataURL to page2 parent
-              //console.log('selectedImage ', this.state.selectedImage)
-        }
-        //read file. Once it is read, onload function will be called
-        reader.readAsDataURL(input);
-      }
-      // return (
-      // <div>
-      //   <button type="button" className="btn btn-success btn"
-      //           onClick={this.onClickHandler}>
-      //           Upload
-      //   </button>
-      //
-      // </div>)
+      return (
+       <div>
+         <button type="button" className="btn btn-success btn"
+                onClick={this.onClickHandler}>
+                Upload
+        </button>
+      
+       </div>)}
     }
+
     render()
     {
         return(
