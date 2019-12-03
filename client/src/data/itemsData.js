c37d3765e276cdda98992dece15b63334421bf36
Jennifer Bernal <jennifer.bernal@ufl.edu>
2019-12-03 11:57:48 -0500

    color look uptable for page 3 display

--- client/src/data/itemsData.js
+++ client/src/data/itemsData.js
@@ -3,7 +3,7 @@ const promoItemsData = [
     name: "16 OZ. KALI SWIGGY STAINLESS STEEL BOTTLE",
     colorsAvailable: [
         //Black, Blue, Lilac, Lime Green, Navy, Red, White.
-        "#000000", "#0000ff","#c8a2c8","#32CD32","#000080","#ff0000", "#ffffff"
+        "#000000", "#0000ff","#c8a2c8","#32CD32","#222953","#ff0000", "#ffffff"
     ],
     capacity: 25,
     unitPrice: 15,
@@ -76,7 +76,7 @@ const apparelItemsData = [
         colorsAvailable: [
             //Black, Bondi blue, dark heather gray, charcoal, espresso, ice blue,
            // indigo, kelly green, midnight navy, mint, orange, purple rush, red,
-            //sand, stone gray, storm, tahiti blue, turquouise, warm gray, white
+            //sand, stone gray, storm blue, tahiti blue, turquouise, warm gray, white
             "#000000", "#0095b6", "#46220b", "#36454f",  "#4e312d", "#d6ecef",
             "#4b0082", "#4cbb17", "#02075d", "#98ff98", "#ffa500","#6A4481", "#FF0000",
             "#c2b280", "#888c8d", "#747880", "#1c87b7", "#40e0d0", "#8c7d70", "#ffffff"
@@ -97,7 +97,7 @@ const apparelItemsData = [
             //Black, white, cardinal, cool blue, heather gray, heavy metal, indigo,
             // midnight navy, military green, red, royal
             "#000000", "##ffffff", "#c41e3a","#2F66A9", "#9aa297", "#46473e",
-            "#4b0082", "#02075d", "#4b5320", "#FF0000", "#4169e1"
+            "#4b0082", "#02075d", "#5a5e45", "#FF0000", "#4169e1"
         ],
         capacity: 10,
         sizeOptions: [ {id:0, name:"S"},{id:1, name:"M"},{id:2, name:"L"},
@@ -115,14 +115,16 @@ const apparelItemsData = [
             True Royal,Navy,Lilac,Team Purple ,Peach, ,Mauve ,Heather Red,
             Red,Heather Maroon ,Gold ,Heather Mustard ,Heathr Sand Dune,
             Tan ,Lt Grey Marble ,Athletic Heather ,Dark Grey ,Dk Gray Heather,
+
             Dk Gry Marb Flc ,Drk Gry Htr/Blk ,Deep Heather ,Dtg Dark Grey,
+
             Black ,Black Heather ,Dtg Black ,Teal ,Forest ,Military Green,
             Ash ,Dtg White ,Vintage White ,White ,Heather Forest ,Heather Navy,
-            Heather Slate ,Heather Tru Royl ,Hthr Deep Teal ,Maroon ,Storm
+            Heather Slate ,Heather Tru Royl ,Hthr Deep Teal ,Maroon ,Storm Purple
              */
             "#0061a5", "#222953", "#efcfe7", "#48267a", "#f6c2ac", "#b96c64", "#e43441",
             "#d43134", "#733a40", "#f8b327", "#dd9526","#e7c5a0", "#bfa284", "#bab7b5",
-            "#999999", "#474246", "#474246", "#6c6f70","#494B48","#363539","#858785",
+            "#999999", "#474246", "#6c6f70", "#6c6f70","#494B48","#363539","#858785",
             "#373538", "#000000", "#1e1e1e", "#141414","#58aa93","#1f3832","#5a5e45",
             "#f3f3f5","#faf9fe", "#ece5df","#FFFFFF", "#67807f", "#556a87","#4C6F82",
             "#285FA2", "#55787a","#5b2a2e","#a39396"
@@ -137,4 +139,80 @@ const apparelItemsData = [
         bundleTotalPrice: 100
     }
 ]
-export {promoItemsData, apparelItemsData}
+const colorRoute =[
+    {name: "Black", color: "#000000"},
+    {name: "Blue", color: "#0000ff"},
+    {name: "Lime Green", color: "#32CD32"},
+    {name: "Orange", color: "#ffa500"},
+    {name: "Purple", color: "#6a0dad"},
+    {name: "Red", color: "#ff0000"},
+    {name: "White", color: "#ffffff"},
+    {name: "Gunmetal", color: "#2a3439"},
+    {name: "Green", color: "#008000"},
+    {name: "Navy", color: "#222953"},
+
+    {name: "Lilac", color: "#efcfe7"},
+    {name: "Gray", color: "#000000"},
+    {name: "Pink", color: "#000000"},
+    {name: "Yellow", color: "#800080"},
+
+    {name: "True Royal", color: "#0061a5"},
+    {name: "Peach", color: "#f6c2ac"},
+    {name: "Mauve", color: "#b96c64"},
+    {name: "Heather Red", color: "#e43441"},
+    {name: "Heather Maroon", color: "#733a40"},
+    {name: "Gold", color: "#f8b327"},
+    {name: "Heather Mustard", color: "#dd9526"},
+    {name: "Heather Sand Dune", color: "#e7c5a0"},
+    {name: "Tan", color: "#bfa284"},
+    {name: "Light Grey Marble", color: "#bab7b5"},
+    {name: "Athletic Heather", color: "#999999"},
+    {name: "Dark Gray", color: "#474246"},
+    {name: "Dark Gray Heather", color: "#6c6f70"},
+    {name: "Dark Gray Marble Flc", color: "#494B48"},
+    {name: "Dark Gray Heather/Black", color: "#363539"},
+    {name: "Deep Heather", color: "#858785"},
+    {name: "Dtg Dark Gray", color: "#373538"},
+
+    {name: "Black Heather", color: "#1e1e1e"},
+    {name: "Dtg Black", color: "#141414"},
+    {name: "Teal", color: "#58aa93"},
+    {name: "Forest", color: "#1f3832"},
+    {name: "Military Green", color: "#5a5e45"},
+    {name: "Ash", color: "#f3f3f5"},
+    {name: "Dtg White", color: "#faf9fe"},
+    {name: "Vintage White", color: "#ece5df"},
+    {name: "Heather Forest", color: "#67807f"},
+    {name: "Heather Navy", color: "#556a87"},
+    {name: "Heather Slate", color: "#4C6F82"},
+    {name: "Heather True Royal", color: "#285FA2"},
+    {name: "Heather Deep Teal", color: "#55787a"},
+    {name: "Maroon", color: "#5b2a2e"},
+    {name: "Storm Purple", color: "#a39396"},
+
+    {name: "Royal Blue", color: "#4169e1"},
+    {name: "Midnight Navy", color: "#02075d"},
+    {name: "Indigo", color: "#4b0082"},
+    {name: "Heavy Metal", color: "#46473e"},
+    {name: "Heather Gray", color: "#9aa297"},
+    {name: "Cardinal", color: "#c41e3a"},
+    {name: "Cool Blue", color: "#2F66A9"},
+
+    {name: "Bondi Blue", color: "#0095b6"},
+    {name: "Dark Heather Gray", color: "#46220b"},
+    {name: "Charcoal", color: "#36454f"},
+    {name: "Espresso", color: "#4e312d"},
+    {name: "Ice Blue", color: "#d6ecef"},
+    {name: "Kelly Green", color: "#4cbb17"},
+    {name: "Mint", color: "#98ff98"},
+    {name: "Purple Rush", color: "#6A4481"},
+
+    {name: "Sand", color: "#c2b280"},
+    {name: "Stone Gray", color: "#888c8d"},
+    {name: "Storm Blue", color: "#747880"},
+    {name: "Tahiti Blue", color: "#1c87b7"},
+    {name: "Turquouise", color: "#40e0d0"},
+    {name: "Warm Gray", color: "#8c7d70"},
+    
+]
+export {promoItemsData, apparelItemsData, colorRoute}
